b8bcd21831724f6ca37d0d8f7e58efe54bb79faa
edmondtm <edmond@letsrotate.com>
2024-08-08 05:42:51 +0800

    listProduct ui

--- frontend/src/app/products/list/list.component.ts
+++ frontend/src/app/products/list/list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../products.interface';
+import { ProductsService } from '../products.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  products: Observable<Array<Product>> = this.productService.getProducts();
+
+  constructor(private productService: ProductsService) {
+    this.products
+  }
 
   ngOnInit(): void {
+    this.products.subscribe(console.log)
+  }
+
+  onDelete(id: number) {
+    this.productService.deleteProduct(id).subscribe(() => {
+      this.products = this.productService.getProducts();
+    });
   }
 
 }
