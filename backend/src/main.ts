1c19afa800ef1e411e2fe834291b4c0c220fb4ef
edmondtm <edmond@letsrotate.com>
2024-08-08 05:24:13 +0800

    add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
@@ -16,7 +17,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
