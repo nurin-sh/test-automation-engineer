222b27c0783ebea5c24e7b749542adf3a1a2f213
edmondtm <edmond@letsrotate.com>
2024-08-07 18:23:41 +0800

    validator and swagger

--- backend/src/orders/dto/create-order.dto.ts
+++ backend/src/orders/dto/create-order.dto.ts
@@ -1,7 +1,14 @@
+import { IsNumber, IsString } from "class-validator";
+
 export class CreateOrderDto {
+    @IsNumber()
     product_id: number;
+    @IsNumber()
     quantity: number;
+    @IsNumber()
     total: number;
+    @IsString()
     created_at: string;
+    @IsString()
     updated_at: string;
 }
