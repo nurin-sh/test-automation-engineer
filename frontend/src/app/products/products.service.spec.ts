62c850223b6c3e51b0b428d369050d4ef68f1e0e
edmondtm <edmond@letsrotate.com>
2024-08-12 09:17:49 +0800

    focus on test in the question

--- frontend/src/app/products/products.service.spec.ts
+++ frontend/src/app/products/products.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { ProductsService } from './products.service';
 
-describe('ProductsService', () => {
+fdescribe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(() => {
